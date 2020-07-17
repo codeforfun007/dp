64e7bb4f5767e17e1c1bd9d598261e92913eba2e
Kamal77-coder <63397944+Kamal77-coder@users.noreply.github.com>
2020-07-17 15:42:30 +0530

    Update trees.cpp

--- trees.cpp
+++ trees.cpp
@@ -25,3 +25,25 @@ bool isSumTree(Node* root)
      
    return solve(root).first;
 }
+// ========gfg :  check if subtree ======
+bool sametree(Node* a,Node* b)
+{
+    if(a == NULL && b== NULL)
+      return true;
+    
+    if(a == NULL || b== NULL)
+      return false;
+    
+    return a->data == b->data &&sametree(a->left,b->left) && sametree(a->right,b->right);
+}
+/*you are required to
+complete this function */
+bool isSubTree(Node* T, Node* S) {
+    // Your code here
+    // return 1 if S is subtree of T else 0
+     if(S == NULL)return true;
+    
+    if(T == NULL)return false;
+    
+    return sametree(T,S)||isSubTree(T->left,S)||isSubTree(T->right,S);
+}
