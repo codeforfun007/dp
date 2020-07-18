c627f8ac6f5b32418c9ea94c2f00a8f597244606
Kamal77-coder <63397944+Kamal77-coder@users.noreply.github.com>
2020-07-18 11:38:19 +0530

    Create dp _int_bit.cpp

---
+++ dp _int_bit.cpp
@@ -0,0 +1,92 @@
+//leetcode 97 : interleaving strings============
+// Input: s1 = "aabcc", s2 = "dbbca", s3 = "aadbbcbcac"
+// Output: true
+
+//Input: s1 = "aabcc", s2 = "dbbca", s3 = "aadbbbaccc"
+//Output: false
+
+ bool isInterleave(string s1, string s2, string s3) {
+        
+        int p = s1.size();
+        int q= s2.size();
+        int r = s3.size();
+        if(p+q != r)  //check1:  this check saves some recursive call
+            return false;
+        vector<vector<int>>dp(p+1 ,vector<int>(q+1,-1));
+        
+        for(int i=0 ;i<=p ;i++)
+        {
+            for(int j=0;j<=q ;j++)
+            {
+                if(i == 0 && j == 0)
+                {
+                     dp[i][j] = true;
+                    continue;
+                         
+                }
+                if( i == 0)
+                {
+                    dp[i][j] = s2[j-1] == s3[i+j-1] && dp[i][j-1];
+                }
+                else if( j == 0)
+                {
+                    dp[i][j] = s1[i-1] == s3[i+j-1] && dp[i-1][j];
+                }
+                else
+                {
+                    dp[i][j] = ((s1[i-1] == s3[i+j-1] && dp[i-1][j] )||(s2[j-1] == s3[i+j-1] && dp[i][j-1]));
+                }
+            }
+            
+        }
+        
+            return dp[p][q];
+        //return solve(p,q,r,s1,s2,s3,dp);
+                                
+    }
+    //going from ending
+    bool solve(int p1,int p2 , int p3,string &s1,string &s2 ,string &s3,vector<vector<int>> &dp)
+    {
+        if(p1 == 0 && p2 == 0 && p3 == 0)
+             return dp[p1][p2] = true;
+        
+       /* if(p3 == 0 || (p3 !=0  && p1 == 0  && p2 == 0)) //if check1 is  there then we don't need this check
+            return dp[p1][p2] = false;*/
+        
+        if(dp[p1][p2] != -1)
+            return dp[p1][p2];
+        
+        if(p1 == 0)
+            return dp[p1][p2] = s2[p2-1] == s3[p3-1] && solve(p1,p2-1,p3-1,s1,s2,s3,dp);
+        
+        if(p2 == 0)
+            return dp[p1][p2] = s1[p1-1] == s3[p3-1] && solve(p1-1 ,p2, p3-1,s1,s2,s3,dp);
+        
+        if((s1[p1-1] == s3[p3-1] && solve(p1-1 , p2, p3-1 ,s1,s2,s3,dp))||
+          s2[p2-1] == s3[p3-1] && solve(p1,p2-1 , p3-1 , s1,s2,s3,dp))
+            return dp[p1][p2] = true;
+        
+        return dp[p1][p2] = false;
+    }
+    //going form starting
+ bool solve(int p1,int p2,int p3 ,string &s1 ,string &s2,string &s3,vector<vector<int>>&dp)
+    {
+         if(p1 == s1.size() && p2 == s2.size() && p3 == s3.size())
+             return true;
+        
+        if(p3 == s3.size())
+            return false;
+        if(dp[p1][p2] != -1)
+            return dp[p1][p2];
+        
+        if(p1 == s1.size())
+            return dp[p1][p2]=(s2[p2] == s3[p3] && solve(p1,p2+1,p3+1,s1,s2,s3,dp));
+        
+        if(p2 == s2.size())
+            return dp[p1][p2] =(s1[p1] == s3[p3] && solve(p1+1,p2,p3+1,s1,s2,s3,dp));
+        
+        if((s1[p1] == s3[p3] && solve(p1+1,p2,p3+1,s1,s2,s3,dp))||s2[p2] == s3[p3]&& solve(p1,p2+1,p3+1 , s1,s2,s3,dp))
+            return dp[p1][p2]=true;
+        
+        return dp[p1][p2]=false;
+    }
