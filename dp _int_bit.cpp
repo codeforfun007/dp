8a09f9f8d84a65d193d199627e33e3347c876abe
Kamal77-coder <63397944+Kamal77-coder@users.noreply.github.com>
2020-07-18 15:44:26 +0530

    Update dp _int_bit.cpp

--- dp _int_bit.cpp
+++ dp _int_bit.cpp
@@ -121,3 +121,65 @@ bool isMatch(string s, string p) {
         else if (i<sn && (p[j]=='.'|| s[i]==p[j]) && isMatch(i+1,s,j+1,p,dp)) return dp[i][j]=1;
         return dp[i][j]= 0;
     }
+
+//scramble string leetcode 81 
+/*Input: s1 = "abcde", s2 = "caebd"
+Output: false*/
+
+/*Below is one possible representation of str = “coder”:
+
+    coder
+   /    \
+  co    der
+ / \    /  \
+c   o  d   er
+           / \
+          e   r
+To scramble the string, we may choose any non-leaf node and swap its two children.
+Suppose, we choose the node "co" and swap its two children, it produces a scrambled string "ocder".*/
+
+bool isScramble(string s1, string s2) {
+        return solve(s1,s2);
+    }
+    
+    bool solve(string s1,string s2)
+    {
+        if(s1.empty() && s2.empty())
+            return true;
+       if(s1 == s2)
+           return true;
+        
+        if(ana(s1,s2) == false)
+            return false;
+        
+        int n = s1.size();
+        
+        for(int i =1;i<n ;i++)
+        {
+            if(solve(s1.substr(0,i),s2.substr(0,i)) && solve(s1.substr(i,n-i),s2.substr(i,n-i)))
+                return true;
+            
+            if(solve(s1.substr(0,i),s2.substr(n-i,i)) && solve(s1.substr(i,n-i),s2.substr(0,n-i)))
+                return true;
+        }
+        
+        return false;
+    }
+    
+    bool ana(string &s,string &p)
+    {
+        if(s.size() != p.size())
+            return false;
+        vector<int>store(26,0);
+        
+        for(int i=0;i<s.size();i++)
+        {
+            store[s[i]-'a']++;
+            store[p[i]-'a']--;
+        }
+        
+        for(int i:store)
+            if(i != 0)return false;
+        
+        return true;
+    }
