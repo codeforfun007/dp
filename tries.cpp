7e37ebf96878b6ab399b916083ecb5e6ce32c3eb
Kamal77-coder <63397944+Kamal77-coder@users.noreply.github.com>
2020-07-24 11:50:08 +0530

    Create tries.cpp

---
+++ tries.cpp
@@ -0,0 +1,154 @@
+//======leetcode 208===========================
+class node
+{
+    public :
+    vector<node*>child;
+    int wordend = 0;
+    
+    node()
+    {
+        this->child.assign(26,NULL);
+        wordend = 0;
+    }
+};
+
+class Trie {
+public:
+    node* root = NULL;
+    /** Initialize your data structure here. */
+    Trie() {
+        
+        root  = new node();
+    }
+    
+    /** Inserts a word into the trie. */
+    void insert(string word) {
+        
+        node* cur = root;
+        int n = word.size();
+        
+        for(int i=0;i<n ;i++)
+        {
+            int idx = word[i]-'a';
+            
+            if(cur->child[idx] == NULL)
+            {
+                cur->child[idx] = new node();
+                
+            }
+            cur = cur->child[idx];
+        }
+        cur->wordend++;
+    }
+    
+    /** Returns if the word is in the trie. */
+    bool search(string word) {
+
+        node* cur = root;
+        int n = word.size();
+        
+        for(int i=0;i<n;i++)
+        {
+            int idx =  word[i]-'a';
+            
+            cur = cur->child[idx];
+            if(cur == NULL)return false;
+        }
+        return cur->wordend>0;
+    }
+    
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    bool startsWith(string prefix) {
+         node* cur = root;
+        int n = prefix.size();
+        
+        for(int i=0;i<n;i++)
+        {
+            int idx =  prefix[i]-'a';
+            
+            cur = cur->child[idx];
+            if(cur == NULL)return false;
+        }
+        return true;
+    }
+};
+
+//== leetcode 211====================================
+ class node{
+    public:
+    int wordend = 0;
+    vector<node*>child;
+    
+    node()
+    {
+        wordend = 0;
+        this->child.assign(26,NULL);
+    }
+};
+
+class WordDictionary {
+public:
+    /** Initialize your data structure here. */
+    node* root = NULL;
+    WordDictionary() {
+        
+        root = new node();
+    }
+    
+    /** Adds a word into the data structure. */
+    void addWord(string word) {
+        node* cur = root;
+        int n = word.size();
+        
+        for(int i=0;i<n ;i++)
+        {
+            int idx = word[i] - 'a';
+            
+            if(cur->child[idx] == NULL)
+            {
+                cur->child[idx] = new node();
+            }
+            cur = cur->child[idx];
+        }
+        cur->wordend++;
+    }
+    bool search_(node* cur , int idx ,string &word)
+    {
+         if(cur == NULL)
+            return false;
+        
+        if(idx == word.size())
+            return cur->wordend>0;
+        
+       
+        
+        bool res = false;
+        if(word[idx] == '.')
+        {
+            for(int i=0;i<26;i++)
+            {
+                if(cur->child[i] != NULL)
+                    res = res||search_(cur->child[i],idx+1,word);
+            }
+        }
+        else
+        {
+            int i = word[idx] - 'a';
+            
+            res = search_(cur->child[i] ,idx+1 , word);
+        }
+        return res;
+    }
+    /** Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. */
+    bool search(string word) {
+      node* cur = root;
+       return search_(cur , 0 , word);
+    }
+};
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary* obj = new WordDictionary();
+ * obj->addWord(word);
+ * bool param_2 = obj->search(word);
+ */
