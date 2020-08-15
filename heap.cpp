11d30e813cf939b0a44470bf9dab8b509c0defad
Kamal77-coder <63397944+Kamal77-coder@users.noreply.github.com>
2020-08-16 00:43:08 +0530

    Update heap.cpp

--- heap.cpp
+++ heap.cpp
@@ -33,3 +33,58 @@
         
         return ans;
     }
+
+//leetcode 871 : minimum number of refueling stops 
+
+//0 < stations[0][0] < stations[1][0] < ... < stations[stations.length-1][0] < target
+//if this constraint would not been there then we would have sorted the refueling stops
+//in increasing order of their distance from starting point
+class Solution {
+public:
+    int minRefuelStops(int target, int startFuel, vector<vector<int>>& stations) {
+        
+        int mypos = startFuel;
+        
+        priority_queue<int>pq ;
+        
+        int stops = 0;
+        for(vector<int>v : stations)
+        {
+            if(v[0] <= mypos)
+            {
+                pq.push(v[1]);
+            }
+            else
+            {
+                if(pq.empty())
+                    break;
+                
+                while(mypos < v[0] && !pq.empty()) //this is an important step
+                 //till you not reach the current refueling stop you can not add the 
+                 //fuel it has to your priority queue
+                {
+                    mypos = mypos + pq.top();
+                    pq.pop();
+                    stops++;
+                }
+                
+                if(mypos <v[0])//if still have not reached the stop then you can not reach the end at all
+                    return -1;
+                pq.push(v[1]);
+            }
+        }
+        
+        while(-1)
+        {
+            if(mypos >= target || pq.empty())
+                break;
+            mypos = mypos + pq.top();
+            pq.pop();
+            stops++;
+        }
+        if(mypos >= target)
+        return stops;
+        
+        return -1;
+    }
+};
